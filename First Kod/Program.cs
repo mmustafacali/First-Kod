a184747ccedae3febef0adfeb9af446f77232447
Musta <Musta@DESKTOP-E4TNQV6>
2023-07-08 16:09:05 +0300

    Proje dosyası ekle.

---
+++ First Kod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Kod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            if (r < 0)
+            {
+                Console.WriteLine($"|{r}| = {r * -1}");
+            }
+            else
+            {
+                Console.WriteLine($" |{r}| = {r}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
